c13470eea35a377d8b6c9b51c496e84fd9cbac6c
anderplacencia <anderson.jose.placencia@bootcamp-upgrade.com>
2024-07-24 20:42:54 +0200

    Prueba con POST realizada, todo OK

--- src/api/controllers/user.controllers.js
+++ src/api/controllers/user.controllers.js
@@ -1,7 +1,25 @@
-const add = () => {
+const User = require("../models/user.model");
+
+const add = async (req, res) => {
+    try{
+        //obtenemos los datos del front
+        const body = req.body;
+        //creamos los datos de usuario con la estructura de models
+        const newUser = new User (body);
+        //guardar datos en la coleccion de BD
+        const createdUser = await newUser.save();
+        console.log("holis");
+        return res.json({
+            succes: true,
+            student: createdUser,
+        });
+    } catch(error){
+        console.log(error);
+    };
     //se obtienen los datos de los usuarios que provengan del front
     //guardar los datos en la coleccion de la BD
     //devolver una respuesta
 };
+
 
 module.exports = {add};
