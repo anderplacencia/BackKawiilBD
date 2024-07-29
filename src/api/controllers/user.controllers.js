71e4dfd650bcad8d8a6a0789b4294e6ffb116ebd
Floren González <florengv6@gmail.com>
2024-07-29 16:47:35 +0200

    login response including user data

--- src/api/controllers/user.controllers.js
+++ src/api/controllers/user.controllers.js
@@ -1,60 +1,66 @@
-const User = require('../models/user.model');
-const bcrypt = require("bcrypt");
-const { validateEmailBD, validatePassword } = require("../../utils/validator");
-const { generateToken } = require("../../utils/jwt")
+const User = require('../models/user.model')
+const bcrypt = require('bcrypt')
+const { validateEmailBD, validatePassword } = require('../../utils/validator')
+const { generateToken } = require('../../utils/jwt')
 
 //se obtienen los datos de los usuarios que provengan del front (1.1)
 //guardar los datos en la coleccion de la BD (1.2)
 //devolver una respuesta (1.3)
 
 const add = async (req, res) => {
-    try{
-        //obtenemos los datos del front (1.1)
-        const body = req.body;
-        const {name, email, password, role} = req.body;
-        //creamos los datos de usuario con la estructura de models
-          const newUser = new User ({
-            name,
-            surname,
-            email,
-            password: password,
-            role: "user"
-          });
-        //En este paso evitamos que se cree un usuario ya existente
-        const findUser = await User.find ({ name: req.body.name })
-        if (findUser.length !== 0) {
-            return res.json({ message: "Este usuario ya existe" })
-        }
-        //guardar datos en la coleccion de BD (1.2)
-        const createdUser = await newUser.save();
-        //validar Email
-        const valEmail = await validateEmailBD(req.body.Email);
-        console.log(valEmail); //Devuelve null si no encuentra el Email en la BD
-        if(!valEmail) {
-          //La contraseña debe cumplir con el patron requerido
-          const valPassword = validatePassword(req.body.password)
-          if(valPassword){
-            //Encriptar la contraseña antes de hacer el registro
-            newUser.password = bcrypt.hashSync(newUser.password, 10);
-            const createdUserP = await newUser.save();
-            return res.status(200).json({success: true, data: createdUserP});
-          }else{
-            return res.status(200).json({success: false, message: "La contraseña no cumple con los parametros"});
-          }
-        }
-        return res.status(200).json({success: false, message: "El Email ya está registrado"});
-        //console.log("holis");
-        // devolver respuesta (1.3)
-        return res.json({
-            succes: true,
-            student: createdUser,
-        });
-    } catch(error){
-        // devolver respuesta (1.3)
-        console.log(error);
-    };
-};
-
+  try {
+    //obtenemos los datos del front (1.1)
+    const body = req.body
+    const { name, email, password, role } = req.body
+    //creamos los datos de usuario con la estructura de models
+    const newUser = new User({
+      name,
+      surname,
+      email,
+      password: password,
+      role: 'user'
+    })
+    //En este paso evitamos que se cree un usuario ya existente
+    const findUser = await User.find({ name: req.body.name })
+    if (findUser.length !== 0) {
+      return res.json({ message: 'Este usuario ya existe' })
+    }
+    //guardar datos en la coleccion de BD (1.2)
+    const createdUser = await newUser.save()
+    //validar Email
+    const valEmail = await validateEmailBD(req.body.Email)
+    console.log(valEmail) //Devuelve null si no encuentra el Email en la BD
+    if (!valEmail) {
+      //La contraseña debe cumplir con el patron requerido
+      const valPassword = validatePassword(req.body.password)
+      if (valPassword) {
+        //Encriptar la contraseña antes de hacer el registro
+        newUser.password = bcrypt.hashSync(newUser.password, 10)
+        const createdUserP = await newUser.save()
+        return res.status(200).json({ success: true, data: createdUserP })
+      } else {
+        return res
+          .status(200)
+          .json({
+            success: false,
+            message: 'La contraseña no cumple con los parametros'
+          })
+      }
+    }
+    return res
+      .status(200)
+      .json({ success: false, message: 'El Email ya está registrado' })
+    //console.log("holis");
+    // devolver respuesta (1.3)
+    return res.json({
+      succes: true,
+      student: createdUser
+    })
+  } catch (error) {
+    // devolver respuesta (1.3)
+    console.log(error)
+  }
+}
 
 // Actualizar perfil del usuario
 const updateProfile = async (req, res) => {
@@ -108,40 +114,50 @@ const getUserById = async (req, res) => {
   }
 }
 
-  //Validaremos el Email
+//Validaremos el Email
 const login = async (req, res) => {
   try {
-    const userBody = req.body;
-    const userDB = await validateEmailBD(userBody.email);
-    if(!userDB){
-      return res.status(200).json({succes: false, message:"El email no esta registrado"})
+    const { email, password } = req.body
+    //buscamos y validamos el usuario por su email
+    const userDB = await validateEmailBD(email)
+    //si no se encuentra usuario:
+    if (!userDB) {
+      return res
+        .status(200)
+        .json({ succes: false, message: 'El email no esta registrado' })
     }
-    if(!bcrypt.compareSync(userBody.password, userDB.password))
-    return res.status(200).json({succes: true, message:"contraseña invalida"})
+    //si no coinciden las ccontraseñas:
+    if (!bcrypt.compareSync(password, userDB.password))
+      return res
+        .status(200)
+        .json({ succes: true, message: 'contraseña invalida' })
 
     //generamos el token
     const token = generateToken({
       name: userDB.name,
       email: userDB.email,
       _id: userDB.id
-
     })
-    return res.status(200).json({succes: true, token: token})
 
-
+    return res.status(200).json({ succes: true, token: token, user: userDB
+    })
   } catch (error) {
     return res.status(500).json(error)
   }
 }
 
 const modifyProfile = async (req, res) => {
-  console.log("función de modificar");
-  console.log(req.userProfile);
-  const newProfile = new Profile(req.body);
-  newProfile.password = bcrypt.hashSync( req.body.password, 10 );
-  newProfile._id = req.userProfile._id;
-  const updateUser = await User.findByIdAndUpdate(req.userProfile._id, newProfile, {new: true} )
-  return res.status(200).json({data:updateUser})
+  console.log('función de modificar')
+  console.log(req.userProfile)
+  const newProfile = new Profile(req.body)
+  newProfile.password = bcrypt.hashSync(req.body.password, 10)
+  newProfile._id = req.userProfile._id
+  const updateUser = await User.findByIdAndUpdate(
+    req.userProfile._id,
+    newProfile,
+    { new: true }
+  )
+  return res.status(200).json({ data: updateUser })
 }
 
 //relacionar una factura con un usuario
@@ -184,7 +200,7 @@ const asignInvoice = async (req, res) => {
 const getUserInvoices = async (req, res) => {
   try {
     const { id } = req.params
-    const findUser = await User.findOne({ _id: id }).populate("userInvoices")
+    const findUser = await User.findOne({ _id: id }).populate('userInvoices')
 
     if (!findUser) {
       return res
@@ -199,5 +215,13 @@ const getUserInvoices = async (req, res) => {
   }
 }
 
-
-module.exports = {add, updateProfile, deleteUserID, getUserById, login, modifyProfile , asignInvoice, getUserInvoices };
+module.exports = {
+  add,
+  updateProfile,
+  deleteUserID,
+  getUserById,
+  login,
+  modifyProfile,
+  asignInvoice,
+  getUserInvoices
+}
